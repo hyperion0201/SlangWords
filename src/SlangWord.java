2680d33c57a0db7528de9f07f31765cfdc0f97d0
Hieu Hoang <megamind.ft@gmail.com>
2021-04-22 09:51:07 +0700

    implement base of SlangWord class

--- src/SlangWord.java
+++ src/SlangWord.java
@@ -1,3 +1,25 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Random;
+
 public class SlangWord {
-    
+    private HashMap<String, List<String>> slangMap;
+    private HashMap<String, List<String>> definitionMap;
+    private ArrayList<String> searchedMap;
+
+    private static final String DATA_PATH = "./data/dictionary.txt";
+    private static final String SEARCH_HISTORY_PATH = "./data/history.txt";
+
+    public SlangWord() {
+        slangMap = new HashMap<String, List<String>>();
+        definitionMap = new HashMap<String, List<String>>();
+        searchedMap = new ArrayList<String>();
+    }
 }
