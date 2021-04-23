8c2abfc9699a73e92e3cd84b7a863e4fe652ddcf
Hieu Hoang <megamind.ft@gmail.com>
2021-04-23 09:51:07 +0700

    implement initial session

--- src/SlangWord.java
+++ src/SlangWord.java
@@ -22,4 +22,65 @@ public class SlangWord {
         definitionMap = new HashMap<String, List<String>>();
         searchedMap = new ArrayList<String>();
     }
+    private void _loadHistorySession() throws Exception {
+        try {
+            var fr = new FileReader(SEARCH_HISTORY_PATH);
+            var br = new BufferedReader(fr);
+            String currentLine = null;
+
+            while ((currentLine = br.readLine()) != null) {
+                searchedMap.add(currentLine.toString());
+            }
+            br.close();
+            fr.close();
+        }
+        catch (Exception e) {
+            System.out.print("No history exist. Launching new session...");
+        }
+
+    }
+    public void _initializeSession() throws Exception {
+        try {
+            _loadHistorySession();
+            var fileReader = new FileReader(DATA_PATH);
+            var bufferReader = new BufferedReader(fileReader);
+            String currentLine = null;
+
+            while ((currentLine = bufferReader.readLine()) != null) {
+                // Slang - Definition list handling
+                var words = currentLine.split("`");
+                var definitions = words[1].split("[|]");
+                _trimingSpaces(definitions);
+                var definitionsList = Arrays.asList(definitions);
+                slangMap.put(words[0], definitionsList);
+
+                // Definition - Slang handling
+                for (String def : definitions) {
+                    definitionMap.put(def, _buildSlangsByDefinition(def, words[0]));
+                }
+            }
+
+            bufferReader.close();
+            fileReader.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+    
+    public void displaySlang() {
+        for (String key : slangMap.keySet()) {
+            System.out.println("Slang : " + key);
+            displayList(slangMap.get(key));
+        }
+    }
+
+    public void displayDef() {
+        for (String key : definitionMap.keySet()) {
+            System.out.println("Def : " + key);
+            displayList(definitionMap.get(key));
+        }
+    }
+
+
 }
